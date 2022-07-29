a819a29143b54d1cf9843ba70b9ecc9fd81e9455
Matthew Shelby <matthewshelB@gmail.com>
2022-07-29 14:17:19 +0430

    Update Hall.js

--- Hall.js
+++ Hall.js
@@ -1,4 +1,3 @@
-import './style.css';
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r99/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
